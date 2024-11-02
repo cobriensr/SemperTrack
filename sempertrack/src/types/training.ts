10e96486d151531a8e27626423f0e9b6d18d9bf7
cobriensr <charles.a.obrien@outlook.com>
2024-11-02 16:29:29 -0500

    rearranged folders

---
+++ sempertrack/src/types/training.ts
@@ -0,0 +1,17 @@
+export interface MOS {
+    code: string;
+    title: string;
+    category: string;
+    description: string;
+    requirements: string[];
+    relatedCivilian: string[];
+}
+
+export interface PromotionPoints {
+    rifle: number;
+    pft: number;
+    cft: number;
+    marineCorpsInstitute: number;
+    offDutyEducation: number;
+    awards: Award[];
+}
